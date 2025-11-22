f72863195dcfb2dfab3ea81250f2cb1c6226407e
Ratnesh Srivastava <ratsri17@gmail.com>
2025-11-22 15:31:04 +0530

    db name change

--- config/db.js
+++ config/db.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 const uri = "mongodb+srv://ratsri17:Rat17Sri@cluster0.cj5jxri.mongodb.net/?appName=Cluster0";
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+const clientOptions = {
+  serverApi: { version: '1', strict: true, deprecationErrors: true },
+  dbName: 'blood-donation-db'
+};
 
 const connectDB = async () => {
   try {
